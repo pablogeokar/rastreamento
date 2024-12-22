81090c759982825eeb3dabdca35742318bf909e7
Pablo George <pablogeokar@hotmail.com>
2024-12-22 10:37:53 -0300

    feat: ajustes no evento de listagem de rotas em tempo real

--- nestjs-api/src/routes/routes-driver/routes-driver.gateway.ts
+++ nestjs-api/src/routes/routes-driver/routes-driver.gateway.ts
@@ -23,7 +23,14 @@ export class RoutesDriverGateway {
 
       await sleep(2000);
       const { lat: lat2, lng: lng2 } = step.end_location;
+
       client.emit(`server:new-points/${route_id}:list`, {
+        route_id,
+        lat: lat2,
+        lng: lng2,
+      });
+
+      client.broadcast.emit("server:new-points:list", {
         route_id,
         lat: lat2,
         lng: lng2,
